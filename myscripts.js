d88d7d1704f146e221ec38b5c629cc6f71d2b9d3
sp8795 <shubhamspandey570@gmail.com>
2021-08-02 22:22:22 +0530

    Google firebase authetication

--- myscripts.js
+++ myscripts.js
@@ -58,3 +58,37 @@ function deleteData(dataKey) {
 
     firebase.database().ref('Tasks/' + dataKey).remove();
 }
+
+
+//Google authentication
+firebase.auth().onAuthStateChanged(firbaseUser => {
+    if (firbaseUser) {
+        console.log(firbaseUser);
+    } else {
+        console.log('Not logged in');
+    }
+})
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
