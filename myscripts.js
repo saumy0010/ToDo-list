305da58235ac47c32f6b8316452edefe39eb2e59
sp8795 <shubhamspandey570@gmail.com>
2021-08-03 16:50:05 +0530

    fixed few bug related to data showing without user logged in

--- myscripts.js
+++ myscripts.js
@@ -12,83 +12,82 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 
-// adding enter key functionality to submit the task.
-
-const form = document.getElementById('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    firebase.database().ref('Tasks').push({
-        task: form['task'].value
-    });
-});
-
-
-
-// Inserting the new task in the list.
-function insertData(id, task) {
-    const parent = document.getElementById('data');
-    const li_element = document.createElement("li");
-    const input_element = document.createElement("input");
-    input_element.type = 'radio';
-    input_element.id = id;
-    li_element.id = id;
-
-    input_element.addEventListener('click', (event) => {
-        deleteData(event.target.id);
-        var val = document.getElementById(`${event.target.id}`);
-        parent.removeChild(val);
-    });
-
-    li_element.innerHTML = task['task'];
-    li_element.appendChild(input_element);
-    parent.appendChild(li_element);
-
-}
-
-// listener to recieve data from firebase RTDB
-firebase.database().ref('Tasks').on('child_added', function(snapshot) {
-    let key = snapshot.key
-    let data = snapshot.val();
-    insertData(key, data);
-});
-
-
-// Delete a task by id
-function deleteData(dataKey) {
-
-    firebase.database().ref('Tasks/' + dataKey).remove();
-}
-
-
 //Google authentication
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+
 firebase.auth().onAuthStateChanged(firbaseUser => {
     if (firbaseUser) {
-        console.log(firbaseUser);
+
+        document.getElementById('data').style.display = 'block';
+        const form = document.getElementById('form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            firebase.database().ref(`${firbaseUser.uid}`).push({
+                task: form['task'].value
+            });
+        });
+
+
+        // listener to recieve data from firebase RTDB
+        firebase.database().ref(`${firbaseUser.uid}`).on('child_added', function(snapshot) {
+            let key = snapshot.key
+            let data = snapshot.val();
+            insertData(key, data);
+        });
+
+        // Inserting the new task in the list.
+        function insertData(id, task) {
+            const parent = document.getElementById('data');
+            const li_element = document.createElement("li");
+            const input_element = document.createElement("input");
+            input_element.type = 'radio';
+            input_element.id = id;
+            li_element.id = id;
+            parent.style = 'background: #fdeeea';
+
+            input_element.addEventListener('click', (event) => {
+                deleteData(event.target.id);
+                var val = document.getElementById(`${event.target.id}`);
+                parent.removeChild(val);
+            });
+
+            li_element.innerHTML = task['task'];
+            li_element.appendChild(input_element);
+            parent.appendChild(li_element);
+        }
+
+        // Delete a task by id
+        function deleteData(dataKey) {
+
+            firebase.database().ref(`${firbaseUser.uid}/` + dataKey).remove();
+        }
+
     } else {
-        console.log('Not logged in');
+
+        document.getElementById('data').style.display = 'none';
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                console.log(credential);
+            });
+
     }
-})
-
-var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
-
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
+});
